2084426c81d6799f408ebf673a32a347cf382161
Asif-Rot <74010095+Asif-Rot@users.noreply.github.com>
2021-05-24 21:39:23 +0300

    first commit

---
+++ Test.cpp
@@ -0,0 +1,101 @@
+#include "doctest.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
+
+#include "BinaryTree.hpp"
+using namespace ariel;
+
+
+TEST_CASE("Good BinaryTree code") {
+    BinaryTree<string> b_string;
+    CHECK_NOTHROW(b_string.add_root("1"));
+    CHECK_NOTHROW(b_string.add_root("2")); // root is now 2
+    CHECK_NOTHROW(b_string.add_left("2", "8"));
+    CHECK_NOTHROW(b_string.add_left("2", "7")); // root 2: left is now 7
+    CHECK_NOTHROW(b_string.add_right("2", "5"));
+    CHECK_NOTHROW(b_string.add_left("7", "9"));
+    CHECK_NOTHROW(b_string.add_right("7", "4")); // parent 7: right is 4
+    /**
+     *         2
+     *       /   \
+     *      7     5
+     *     / \
+     *    9   4
+     */
+
+    string preorderArr[] = {"2", "7", "9", "4", "5"};
+    string inorderArr[] = {"9", "7", "4", "2", "5"};
+    string postorderArr[] = {"9", "4", "7", "5", "2"};
+
+    int i = 0;
+    for (auto it=b_string.begin_preorder(); it!=b_string.end_preorder(); ++it) {
+        CHECK((*it) == preorderArr[i]);
+        i++;
+    }
+
+    i = 0;
+    for (auto it=b_string.begin_inorder(); it!=b_string.end_inorder(); ++it) {
+        CHECK((*it) == inorderArr[i]);
+        i++;
+    }
+
+    i = 0;
+    for (auto it=b_string.begin_postorder(); it!=b_string.end_postorder(); ++it) {
+        CHECK((*it) == postorderArr[i]);
+        i++;
+    }
+
+    // demonstrate the arrow operator:
+    i = 0;
+    for (auto it=b_string.begin_postorder(); it!=b_string.end_postorder(); ++it) {
+        CHECK(it->size() == postorderArr[i].size());
+        i++;
+    }
+
+    i = 0;
+    for (const string& element: b_string) {  // this should work like inorder
+        CHECK(element == inorderArr[i]);
+        i++;
+    }  
+    
+}
+
+
+TEST_CASE("Bad BinaryTree code") {
+    BinaryTree<int> b_int;
+    CHECK_NOTHROW(b_int.add_root(1));
+    CHECK_NOTHROW(b_int.add_root(1)); // same value twice
+    CHECK_THROWS(b_int.add_left(2, 8)); // 2 is not the root
+    CHECK_NOTHROW(b_int.add_right(1, 5));
+    CHECK_THROWS(b_int.add_right(4, 2)); // 4 is not a parent in the tree
+    CHECK_NOTHROW(b_int.add_root(4)); // Change root to 4
+    CHECK_THROWS(b_int.add_left(1, 8)); // 1 is not the root anymore
+    CHECK_THROWS(b_int.add_right(1, 3)); // 1 is not the root anymore
+    CHECK_NOTHROW(b_int.add_right(4, 2)); // Can change to 2!
+    CHECK_THROWS(b_int.add_right(5, 6)); // no 5
+    CHECK_THROWS(b_int.add_left(19, 42));// no 19
+    CHECK_NOTHROW(b_int.add_root(7));
+    CHECK_NOTHROW(b_int.add_left(7, 6));
+
+    /**
+     *          7
+     *        /   \
+     *       6     2
+     */
+
+    int postorderArr[] = {6, 2, 7};
+
+    int i = 0;
+    bool c;
+    for (auto it=b_int.begin_preorder(); it!=b_int.end_preorder(); ++it) {
+        CHECK_FALSE((*it) == postorderArr[i]);
+        i++;
+    }
+
+    CHECK_THROWS(b_int.add_right(4, 6)); // 4 is not the root anymore
+    CHECK_NOTHROW(cout << b_int << endl);
+
+}
---
+++ sources/BinaryTree.hpp
@@ -0,0 +1,285 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+namespace ariel {
+
+    template<class T>
+    class BinaryTree {
+
+        struct Node {
+            T data;
+            Node* left = nullptr;
+            Node* right = nullptr;
+            Node(T a): data(a), left(nullptr), right(nullptr) {}
+        };
+        Node *root = nullptr;
+        
+        public:
+            BinaryTree<T> () {}
+
+            BinaryTree<T> search_parent(const T& parent, const T& child, bool isleft) {
+                if (root == NULL) {
+                    throw invalid_argument("No such root");
+                }
+
+                stack<Node*> nodeStack_search;
+                Node *node = root;
+            
+                while (node != nullptr || !nodeStack_search.empty()) {
+                    while (node != nullptr) {
+                        nodeStack_search.push(node);
+                        node = node->left;
+                    }
+                    node = nodeStack_search.top();
+                    nodeStack_search.pop();
+
+                    if (node->data == parent) {
+                        if (isleft) {
+                            if(node->left == nullptr) {
+                                node->left = new Node(child);
+                            } else {
+                                node->left->data = child;
+                            }
+                        }
+                        else {
+                            if(node->right == nullptr) {
+                                node->right = new Node(child);
+                            } else {
+                                node->right->data = child;
+                            }
+                        }
+                        return *this;
+                    }
+
+                    node = node->right;
+                }
+                throw invalid_argument("There is no such parent");
+            }
+
+
+            BinaryTree<T> add_root(const T& data) {
+                if (root == nullptr) {
+                    root = new Node(data);
+                } else {
+                    root->data = data;
+                }
+                return *this;
+            }
+
+            BinaryTree<T> add_left(const T& rootdata, const T& leftdata) {
+                search_parent(rootdata, leftdata, true);
+                return *this;
+            }
+
+            BinaryTree<T> add_right(const T& rootdata, const T& rightdata) {
+                search_parent(rootdata, rightdata, false);
+                return *this;
+            }
+
+            class iterator{
+
+            private:
+                int orderof;
+                stack<Node*> nodeStack;
+                queue<Node*> q_print;
+                stack<Node*> s_print;
+                Node* p_node;
+
+            public:
+
+                iterator(int order, Node* ptr=nullptr) : p_node(ptr) {
+                    orderof = order;
+                    if(p_node != nullptr){
+                        
+                        if(order == 0){ //preorder
+                            if(p_node->right != nullptr){
+                                nodeStack.push(p_node->right);
+                            }
+                            if(p_node->left!=nullptr){
+                                nodeStack.push(p_node->left);
+                            }
+                        }
+                        else if(order == 1){ //inorder
+                            Node *tmp = p_node;
+
+                            while (tmp != NULL || !nodeStack.empty()) {
+                                while (tmp !=  NULL) {
+                                    nodeStack.push(tmp);
+                                    tmp = tmp->left;
+                                }
+                
+                                tmp = nodeStack.top();
+                                
+                                q_print.push(tmp);
+                                nodeStack.pop();
+                        
+                                tmp = tmp->right;
+                            }
+                            p_node = q_print.front();
+                            q_print.pop();
+                        }                            
+                        
+                        else {
+                            nodeStack.push(p_node); 
+                            while (!nodeStack.empty()){
+                                Node* tmp = nodeStack.top();
+                                nodeStack.pop();
+                                s_print.push(tmp);
+                                if (tmp->left) {
+                                    nodeStack.push(tmp->left);
+                                }
+                                if (tmp->right) {
+                                    nodeStack.push(tmp->right);
+                                }
+                            }
+                            p_node = s_print.top();
+                            s_print.pop();
+                        }    
+                    }
+                }
+
+                T& operator*() const {
+                    return p_node->data;
+                }
+
+                T* operator->() const {
+                    return &(p_node->data);
+                }
+
+                // ++i;
+                iterator& operator++() {
+                    if(!nodeStack.empty() || !q_print.empty() || !s_print.empty()){
+
+                        if(orderof == 0){ // preorder
+                            p_node = nodeStack.top();
+                            nodeStack.pop();
+                            if(p_node->right != nullptr){
+                                nodeStack.push(p_node->right);
+                            }
+                            if(p_node->left != nullptr){
+                                nodeStack.push(p_node->left);
+                            }
+                            return *this;
+                        }
+
+                        else if(orderof == 1){ // inorder
+                            p_node = q_print.front();
+                            q_print.pop();
+                            return *this;
+                        }
+
+                        else {
+                            p_node = s_print.top();
+                            s_print.pop();
+                            return *this;
+                        }
+                    }
+
+                    p_node = nullptr;
+                    return *this;
+                }
+                    
+                // i++;
+                const iterator operator++(int) {
+                    iterator tmp = *this;
+                        if(!nodeStack.empty() || !q_print.empty() || !s_print.empty()){
+
+                        if(orderof == 0){ // preorder
+                            p_node = nodeStack.top();
+                            nodeStack.pop();
+                            if(p_node->right != nullptr){
+                                nodeStack.push(p_node->right);
+                            }
+                            if(p_node->left != nullptr){
+                                nodeStack.push(p_node->left);
+                            }
+                            return tmp;
+                        }
+
+                        else if(orderof == 1){ // inorder
+                            p_node = q_print.front();
+                            q_print.pop();
+                            return tmp;
+                        }
+
+                        else {
+                            p_node = s_print.top();
+                            s_print.pop();
+                            return tmp;
+                        }
+                    }
+
+                    p_node = nullptr;
+                    return tmp;
+                }
+
+                bool operator==(const iterator& rhs) const {
+                    return p_node == rhs.p_node;
+                }
+
+                bool operator!=(const iterator& rhs) const {
+                    return p_node != rhs.p_node;
+                }
+            };
+
+            iterator begin_preorder() {
+                return iterator{0, root};
+            }
+
+            iterator end_preorder() {
+                return iterator{0, nullptr};
+            }
+
+            iterator begin_inorder() {
+                return iterator{1, root};
+            }
+            
+            iterator end_inorder() {
+                return iterator{1, nullptr};
+            }
+
+            iterator begin_postorder() {
+                return iterator{2, root};
+            }
+            
+            iterator end_postorder() {
+                return iterator{2, nullptr};
+            }
+
+            iterator begin() { // Need to change to inorder
+                return iterator{1, root};
+            }
+            
+            iterator end() { // Need to change to inorder
+                return iterator{1, nullptr};
+            }
+
+            friend std::ostream& operator<< (std::ostream& os, const BinaryTree<T>& b) {
+                if (b.root == NULL)
+                    return os;
+ 
+                stack<Node*> nodeStack;
+                nodeStack.push(b.root);
+            
+                while (nodeStack.empty() == false) {
+                    Node* node = nodeStack.top();
+                    os << node->data << " ";
+                    nodeStack.pop();
+            
+                    if (node->right)
+                        nodeStack.push(node->right);
+                    if (node->left)
+                        nodeStack.push(node->left);
+                }
+
+                return os;
+            }
+
+    };
+};
